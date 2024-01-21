9495a4b50206289786a11fffeb4928ff60fc2b8e
valterbs86 <148062515+valterbs86@users.noreply.github.com>
2024-01-21 21:51:07 +0000

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,18 @@
 function LoadData() {			
 		d3.csv("./data/Transformed - Population_Income.csv", function(data){
 			//console.log(data);
-			writeData(data)
+			d3.select("body")
+        			.selectAll("p")
+        			.data(data)
+        			.enter()
+        			.append("p")
+        			.text(function(d) {
+            				return d.MaxYear;
+        			});
 		});
 }
 
 function writeData()
 {
-	d3.select("body")
-        .selectAll("p")
-        .data(data)
-        .enter()
-        .append("p")
-        .text(function(d) {
-            return d.name + ", " + d.location;
-        });
 
 }
