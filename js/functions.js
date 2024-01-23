f857ff73ac3e5a12e6278ef3092edcb3f6266718
valterbs86 <148062515+valterbs86@users.noreply.github.com>
2024-01-23 17:28:01 +0100

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -5,23 +5,12 @@ function LoadData()
 		d3.csv("./data/Transformed - Population_Income.csv", function(data){
 			let dataset = data;
 		});
+		
 writeData();
 	
 }
 
 function writeData()
 {
-		let options = [...new Set(dataset.map(d => d.MaxYear))]; 
-		// optionally add .sort() to the end of that line to sort the unique values
-		// alphabetically rather than by insertion order
-
-		d3.select('#my_dataviz')
-  			.selectAll('option')
-    			.data(options)
-  			.enter()
-    			.append('option')
-    			.text(d => d)
-    			.attr('value', d => d);
-
-			//console.log(data);
+	console.log(dataset);
 }
