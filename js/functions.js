d013a730535262128afda510c8b3ce8d60927752
valterbs86 <148062515+valterbs86@users.noreply.github.com>
2024-01-24 18:17:43 +0100

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -13,5 +13,16 @@ function LoadData()
 function PopulateDiv(data)
 {
 	//window.alert(JSON.parse(localStorage.getItem('dataset')));
-	window.alert(data);
+
+	let options = [...new Set(data.map(d => d.MaxYear))]; 
+		// optionally add .sort() to the end of that line to sort the unique values
+		// alphabetically rather than by insertion order
+
+		d3.select('#my_dataviz')
+  			.selectAll('option')
+    			.data(options)
+  			.enter()
+    			.append('option')
+    			.text(d => d)
+    			.attr('value', d => d);
 }
