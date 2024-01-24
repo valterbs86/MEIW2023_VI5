b03b942c441442256e530933fb4c80ec23a178e0
valterbs86 <148062515+valterbs86@users.noreply.github.com>
2024-01-24 18:00:39 +0100

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -3,12 +3,11 @@ function LoadData()
 {
 	console.log('Loading Data...');	
 	d3.csv("./data/Transformed - Population_Income.csv").then(function(data) {
-		localStorage.setItem('dataset', data)
+		localStorage.setItem('dataset', JSON.stringify(data));
 	});
 }
 
 function ShowData()
 {
-	window.alert('Output Data...');
-	
+	window.alert(JSON.parse(localStorage.getItem('dataset')));
 }
