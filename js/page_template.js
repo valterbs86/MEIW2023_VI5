8250ecaca2ba72d92ce11ca0097264c7d9938619
valterbs86 <148062515+valterbs86@users.noreply.github.com>
2024-02-07 19:37:58 +0000

    Add files via upload

--- js/page_template.js
+++ js/page_template.js
@@ -13,12 +13,18 @@ function slideMenu() {
 }
 
 function scrollToDiv(div) {
-		let text1 = ".";
-		let element = text1.concat(div);
-		var targetDiv = document.querySelector(element);
-		
-		targetDiv.scrollIntoView({ behavior: 'smooth' });
-		
+    let text1 = ".";
+    let element = text1.concat(div);
+    var targetDiv = document.querySelector(element);
+    
+    // Calculate the offset to scroll above the target div
+    var scrollPosition = targetDiv.offsetTop - 50;
+
+    // Scroll to the calculated position
+    window.scrollTo({
+        top: scrollPosition,
+        behavior: 'smooth'
+    });
 }
 
 $(function() {
